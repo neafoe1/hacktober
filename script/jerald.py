e146f988364323615057b0f9cbc1cfef98ce5661
Jerald <jeraldssw@gmail.com>
2020-10-18 01:45:15 +0800

    New contribution for Python

--- script/jerald.py
+++ script/jerald.py
@@ -1,5 +1,10 @@
 # Simplest way to print Hello World in Python
 
+# LANGUAGE: Python
+# ENV: Script.py
+# AUTHOR: Jerald
+# GITHUB: https:#github.com/jeraldlyh
+
 def main():
     print('Hello World')
 
